d28cbf1fd1a659cd1ab317da83f291d675024335
lorenzo <lorenzo.cerrone@uzh.ch>
2025-05-13 15:59:53 +0200

    upload whl and pwhl

--- 1_2_3_create_pixi_env.sh
+++ 1_2_3_create_pixi_env.sh
@@ -1,6 +1,6 @@
 
-PACKAGE_ENV_DIR="./pwhl_env"
-PWHL_PATH="./dist/ilastik_tasks-0.1.dev28+g6125ccc.pwhl.zip"
+PACKAGE_ENV_DIR="/tmp/pixi_env"
+PWHL_PATH="dist/ilastik_tasks-0.1.pwhl.zip"
 ENV_EXTRA="dev" # Specify extra in pixi are installed in a separete env (dev, test, task-core)
 
 MANIFEST_PATH="${PACKAGE_ENV_DIR}/pyproject.toml"
--- 6_pixi_install_from_lock.sh
+++ 6_pixi_install_from_lock.sh
@@ -1,6 +1,6 @@
-PACKAGE_ENV_DIR="./pwhl_env"
+PACKAGE_ENV_DIR="/tmp/pixi_env"
 MANIFEST_PATH="${PACKAGE_ENV_DIR}/pyproject.toml"
-ENV_EXTRA="default" # Specify extra in pixi are installed in a separete env (dev, test, task-core)
+ENV_EXTRA="dev" # Specify extra in pixi are installed in a separete env (dev, test, task-core)
 
 # Create the environment using pixi
 pixi install --manifest-path $MANIFEST_PATH -e $ENV_EXTRA --frozen
